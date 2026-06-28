37fe8333fd966c9eb0ee363884a11a2944821b6a
ihsankhan-752 <ihsan@example.com>
2026-06-28 10:53:41 +0500

    User Routes Added

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,16 @@
 import express from "express";
-import { register } from "../controllers/auth.controller.js";
+import {
+  register,
+  login,
+  logout,
+  getMe,
+} from "../controllers/auth.controller.js";
+
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 const router = express.Router();
 
 router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", authMiddleware, logout);
+router.get("/me", authMiddleware, getMe);
 export default router;
